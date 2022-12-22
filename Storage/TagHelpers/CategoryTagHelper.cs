34edd9b83d5fd17f0588dd16dfb5dd839d10d4aa
JYM <jeanyvesmichel@yahoo.com>
2022-12-22 16:09:34 +0100

    New TagHelpers level Added

---
+++ Storage/TagHelpers/CategoryTagHelper.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Text.Encodings.Web;
+
+namespace Storage.TagHelpers
+{
+    [HtmlTargetElement("level")]
+    public class CategoryTagHelper : TagHelper
+    {
+        public int Category { get; set; }
+       
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "span";
+            //output.AddClass("level", HtmlEncoder.Default);
+
+            if (Category == 0 || Category == 32)
+            {
+                output.Content.SetHtmlContent("Store in section N/A".ToString());
+            }
+            else if (Category == 7 || Category == 8)
+            {
+                output.Content.SetHtmlContent("Store in section IR".ToString());
+            }
+            else
+            {
+                output.Content.SetHtmlContent("Place the item label on the right hand side and Store".ToString());
+            }
+
+        }
+    }
+}
